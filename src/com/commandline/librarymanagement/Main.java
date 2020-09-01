50864470b9cbca932f83b3cfee9e5443da689304
Sohan Kotha <sohankotha@gmail.com>
2020-09-02 02:44:07 +0530

    Finalized Output with a jar executable

--- src/com/commandline/librarymanagement/Main.java
+++ src/com/commandline/librarymanagement/Main.java
@@ -15,7 +15,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        BookManagement bookManagement = new BookManagement();
+        BookManagement  bookManagement = new BookManagement();
         StudentManagement studentManagement = new StudentManagement();
         FacultyManagement facultyManagement = new FacultyManagement();
         while (flag) {
